fa3f92052a2f32fa27587d9c9c724c11d85d345b
Alexandre Fourcat <afourcat@gmail.com>
2020-01-15 18:10:54 +0100

    :white_check_mark: Adding test for Parsing.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+
+import           KoakParser
+import           Parser
+import           Expression
+import           Test.Hspec
+import           Control.Exception              ( evaluate )
+
+
+runKoakParser = runParser koak
+
+functionDefinitionKoak = "def test(x: double): double x + 2.0;"
+functionCallKoak = "test(5.0);"
+assignementKoak = "y = 5;"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Function definition." $ do
+        it "Parse a function definition." $ do
+            let value = runKoakParser functionDefinitionKoak
+            value
+                `shouldBe` (Right
+                               (Def
+                                   (DefineFunction
+                                       "test"
+                                       [Variable "x" (Just Double)]
+                                       (Just Double)
+                                       (BinaryOp "+" (Var "x") (Float 2.0))
+                                   )
+                               )
+                           )
+
+        it "Parse a function call." $ do
+            let value = runKoakParser functionCallKoak
+            value `shouldBe` (Right (Call "test" [Float 5.0]))
+
+        it "Parse an assignement of an int." $ do
+            let value = runKoakParser assignementKoak
+            value `shouldBe` (Right (BinaryOp "=" (Var "y") (Int 5)))
