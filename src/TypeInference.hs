b3a9c170c5435766c6de0d05a99bbfe04c7e8c11
Alexandre Fourcat <afourcat@gmail.com>
2020-02-20 16:12:43 +0100

    :white_check_mark: Adding test for type inference.

--- src/TypeInference.hs
+++ src/TypeInference.hs
@@ -1,24 +1,59 @@
 module TypeInference
-    ()
+    ( infereType
+    )
 where
 
+import           Debug.Trace
 import           State
 import           AST
-import           Data.Map
+import qualified Data.Map                      as Map
 
 type Error = String
 
--- Type context.
-type TypeState = StateT (Map String KType) (Either Error) [KDefs]
+type Context = (Map.Map String (Maybe KType))
 
-infereType :: [KDefs] -> [KDefs]
-infereType = annotate . collectConstraints . unify
+infereType :: [KDefs] -> Either Error [KDefs]
+infereType e = unify e . collectConstraints e . annotate $ e
 
-annotate :: [KDefs] -> [KDefs]
-annotate = id
+annotate :: [KDefs] -> Context
+annotate = foldr annotateKdef Map.empty
 
-collectConstraints :: [KDefs] -> [KDefs]
-collectConstraints = id
+annotateKdef :: KDefs -> Context -> Context
+annotateKdef kdef ctx = case kdef of
+    Def name vars ty expr -> Map.insert name ty (annotateVariables vars ctx) -- TODO change
+    _                     -> ctx
 
-unify :: [KDefs] -> [KDefs]
-unify = id
+annotateVariables :: [VariableDef] -> Context -> Context
+annotateVariables vars ctx = foldr annotateVariableDef ctx vars
+
+annotateVariableDef :: VariableDef -> Context -> Context
+annotateVariableDef (VariableDef name ty) = Map.insert name ty
+
+annotateKExprs :: KExprs -> Context -> Context
+annotateKExprs kexprs ctx = case kexprs of
+    For var1 val1 cond1 cond2 step exprs -> ctx
+    If cond action elseAction            -> ctx
+    While cond action                    -> ctx
+    Expression listExpr                  -> ctx
+
+
+annotateKExpr :: Context -> KExpr -> Context
+annotateKExpr ctx kexpr = case kexpr of
+    Int   x                  -> ctx
+    Float y                  -> ctx
+    BinaryOp name expr expr1 -> annotateBinary name expr expr1 ctx
+    UnaryOp name expr        -> ctx
+    Identifier name          -> ctx
+    Call expr listExpr       -> ctx
+    Primary exprs            -> annotateKExprs exprs ctx
+
+annotateBinary :: String -> KExpr -> KExpr -> Context -> Context
+annotateBinary name ex1 ex2 ctx = case ex1 of
+    Identifier x -> Map.insert x Nothing ctx
+    _            -> ctx
+
+collectConstraints :: [KDefs] -> Context -> Context
+collectConstraints expr ctx = trace (concatMap show ctx) ctx
+
+unify :: [KDefs] -> Context -> Either Error [KDefs]
+unify expr ctx = Right []
---
+++ test/InferenceSpec.hs
@@ -0,0 +1,117 @@
+module InferenceSpec where
+import           TypeInference
+import           AST
+import           Test.Hspec
+import           Control.Exception              ( evaluate )
+import           Debug.Trace
+
+simpleFunction = -- def test(x) x + 2.0;
+    [ Def "test"
+          [VariableDef "x" Nothing]
+          Nothing
+          (Expression [BinaryOp "+" (Identifier "x") (Float 2.0)])
+    ]
+
+simpleFunction2 = -- def test(x, y: int) x + y;
+    [ Def "test"
+          [VariableDef "x" Nothing, VariableDef "y" (Just TInteger)]
+          Nothing
+          (Expression [BinaryOp "+" (Identifier "y") (Identifier "x")])
+    ]
+
+simpleFunction3 = -- def test(x, y, z: int) x + y + 2.0;
+    [ Def
+          "test"
+          [ VariableDef "x" Nothing
+          , VariableDef "y" Nothing
+          , VariableDef "z" (Just TInteger)
+          ]
+          Nothing
+          (Expression
+              [ BinaryOp "+"
+                         (Float 2.0)
+                         (BinaryOp "+" (Identifier "x") (Identifier "y"))
+              ]
+          )
+    ]
+
+multipleValues =
+    [ Expressions $ Expression [BinaryOp "=" (Identifier "a") (Int 50)]
+    , Expressions $ Expression [BinaryOp "=" (Identifier "i") (Int 0)]
+    , Expressions $ While
+        (BinaryOp "<" (Identifier "i") (Identifier "a"))
+        (Expression
+            [ BinaryOp "="
+                       (Identifier "b")
+                       (BinaryOp "+" (Identifier "i") (Int 10))
+            , Call (Identifier "print") [BinaryOp "+" (Identifier "b") (Int 10)]
+            ]
+        )
+    , Expressions $ Expression [Call (Identifier "print") [Identifier "a"]]
+    , Expressions $ Expression [Call (Identifier "print") [Identifier "i"]]
+    ]
+
+noInferenceFunction = -- def test(x: int, y: int): int x + y + 1; -- no inference 
+    [ Def "test"
+          [VariableDef "x" (Just TInteger), VariableDef "y" (Just TInteger)]
+          (Just TInteger)
+          (Expression [BinaryOp "+" (Identifier "y") (Identifier "x")])
+    ]
+
+
+spec :: Spec
+spec = do
+    describe "Edge case" $ do
+        it "Empty tree" $ infereType [] `shouldBe` Right []
+        it "No inference"
+            $          infereType noInferenceFunction
+            `shouldBe` Right noInferenceFunction
+    describe "Function inference" $ do
+        it "Basic function inference 0"
+            $          infereType simpleFunction
+            `shouldBe` Right
+                           [ Def
+                                 "test"
+                                 [VariableDef "x" (Just TDouble)]
+                                 (Just TDouble)
+                                 (Expression
+                                     [BinaryOp "+" (Identifier "x") (Float 2.0)]
+                                 )
+                           ]
+        it "Basic function inference 1"
+            $          infereType simpleFunction2
+            `shouldBe` Right
+                           [ Def
+                                 "test"
+                                 [ VariableDef "x" (Just TInteger)
+                                 , VariableDef "y" (Just TInteger)
+                                 ]
+                                 (Just TInteger)
+                                 (Expression
+                                     [ BinaryOp "+"
+                                                (Identifier "y")
+                                                (Identifier "x")
+                                     ]
+                                 )
+                           ]
+        it "Basic function inference 2"
+            $          infereType simpleFunction3
+            `shouldBe` Right
+                           [ Def
+                                 "test"
+                                 [ VariableDef "x" (Just TDouble)
+                                 , VariableDef "y" (Just TDouble)
+                                 , VariableDef "z" (Just TInteger)
+                                 ]
+                                 (Just TDouble)
+                                 (Expression
+                                     [ BinaryOp
+                                           "+"
+                                           (Float 2.0)
+                                           (BinaryOp "+"
+                                                     (Identifier "x")
+                                                     (Identifier "y")
+                                           )
+                                     ]
+                                 )
+                           ]
